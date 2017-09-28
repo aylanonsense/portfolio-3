069190116dddb108b81da69fb506f5424a6e276e
bridgs <bridgs.dev@gmail.com>
2017-09-28 17:13:57 -0400

    Minifying html, css, and js

--- scripts/buildHtml.js
+++ scripts/buildHtml.js
@@ -1,8 +1,98 @@
 import Mustache from 'mustache';
+import { minify } from 'html-minifier';
 import loadFile from './helper/loadFile';
 import saveFile from './helper/saveFile';
 import config from '../data/config.json';
 import pixels from '../data/pixels.json';
+
+const minifyOptions = {
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	minifyCSS: {
+		level: 2
+	},
+	minifyJS: {
+		compress: {
+			sequences: true,
+			properties: true,
+			dead_code: true,
+			drop_debugger: true,
+			unsafe: true,
+			unsafe_comps: true,
+			unsafe_Func: true,
+			unsafe_math: true,
+			unsafe_proto: true,
+			unsafe_regexp: true,
+			conditionals: true,
+			comparisons: true,
+			evaluate: true,
+			booleans: true,
+			typeofs: true,
+			loops: true,
+			unused: true,
+			toplevel: false,
+			top_retain: false,
+			hoist_funs: true,
+			hoist_vars: false,
+			if_return: true,
+			inline: true,
+			join_vars: true,
+			cascade: true,
+			collapse_vars: true,
+			reduce_vars: true,
+			warnings: false,
+			negate_iife: true,
+			pure_getters: true,
+			pure_funcs: null,
+			drop_console: true,
+			expression: false,
+			keep_fargs: false,
+			keep_fnames: false,
+			passes: 1,
+			keep_infinity: false,
+			side_effects: true
+		},
+		mangle: {
+			reserved: [],
+			toplevel: true,
+			keep_fnames: false,
+			eval: true
+		},
+		output: {
+			ascii_only: false,
+			beautify: false,
+			bracketize: false,
+			comments: false,
+			indent_level: 4,
+			indent_start: 0,
+			inline_script: false,
+			keep_quoted_props: false,
+			max_line_len: false,
+			preamble: null,
+			preserve_line: false,
+			quote_keys: false,
+			quote_style: 0,
+			semicolons: true,
+			shebang: false,
+			wrap_iife: false
+		},
+		sourceMap: false,
+		toplevel: true,
+		warnings: true
+	},
+	minifyURLs: true,
+	removeAttributeQuotes: true,
+	removeComments: true,
+	removeEmptyAttributes: true,
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true,
+	useShortDoctype: true,
+};
 
 let isLoaded = false;
 let templates;
@@ -58,21 +148,10 @@ export async function buildGalleryHtml(galleryData, projects, buildOptions) {
 		maxCols: config.grid.maxCols
 	};
 	let projectsJSON = Object.values(projects)
-		.map(projectData => {
-			return {
-				id: projectData.id,
-				coordinates: projectData.grid.coordinates
-			};
-		});
+		.map(projectData => [ projectData.id, projectData.grid.coordinates ]);
 	let animatedProjectsJSON = Object.values(projects)
 		.filter(projectData => projectData.image.animated)
-		.map(projectData => {
-			return {
-				id: projectData.id,
-				scale: projectData.grid.scale,
-				imageUri: projectData.image.project.uri
-			};
-		});
+		.map(projectData => [ projectData.id, projectData.grid.scale, projectData.image.project.uri ]);
 	let html = Mustache.render(templates.base, {
 		...siteData,
 		...buildOptions,
@@ -94,7 +173,8 @@ export async function buildGalleryHtml(galleryData, projects, buildOptions) {
 		script: scripts.gallery,
 		style: styles.fontRaleway + styles.universal + styles.gallery
 	});
-	await saveFile(`build/public/${galleryData.uri}.html`, html);
+	let minifiedHtml = minify(html, minifyOptions);
+	await saveFile(`build/public/${galleryData.uri}.html`, minifiedHtml);
 }
 
 export async function buildProjectHtml(galleryData, projectData, buildOptions) {
@@ -118,5 +198,6 @@ export async function buildProjectHtml(galleryData, projectData, buildOptions) {
 		script: null,
 		style: styles.fontRaleway + styles.universal + styles.project
 	});
-	await saveFile(`build/public/${galleryData.uri}/${projectData.project}.html`, html);
+	let minifiedHtml = minify(html, minifyOptions);
+	await saveFile(`build/public/${galleryData.uri}/${projectData.project}.html`, minifiedHtml);
 };
--- web-assets/scripts/gallery.js
+++ web-assets/scripts/gallery.js
@@ -1,22 +1,25 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', function() {
+	var project, id;
+
 	// data
-	const config = ({{{configJSON}}});
-	const gridHeights = ({{{gridHeightsJSON}}});
-	const projects = ({{{projectsJSON}}})
-	const animatedProjects = ({{{animatedProjectsJSON}}});
+	var config = ({{{configJSON}}});
+	var gridHeights = ({{{gridHeightsJSON}}});
+	var projects = ({{{projectsJSON}}})
+	var animatedProjects = ({{{animatedProjectsJSON}}});
 
 	// get elements
-	let galleryEle = document.getElementById('gallery');
-	let projectEles = {};
-	for (let projectData of projects) {
-		projectEles[projectData.id] = document.getElementById(projectData.id);
+	var galleryEle = document.getElementById('gallery');
+	var projectEles = {};
+	for (project in projects) {
+		id = projects[project][0];
+		projectEles[id] = document.getElementById(id);
 	}
 
 	// resize grid to match viewport
-	let currNumCols = config.defaultCols;
+	var currNumCols = config.defaultCols;
 	function considerAdjustingGrid() {
-		let width = document.documentElement.clientWidth;
-		let numCols = Math.floor((width - config.cellGap) / (config.cellWidth + config.cellGap));
+		var width = document.documentElement.clientWidth;
+		var numCols = Math.floor((width - config.cellGap) / (config.cellWidth + config.cellGap));
 		numCols = Math.min(Math.max(config.minCols, config.colStep * Math.floor(numCols / config.colStep)), config.maxCols);
 		if (currNumCols !== numCols) {
 			currNumCols = numCols;
@@ -24,22 +27,24 @@ window.addEventListener('load', () => {
 		}
 	}
 	function adjustGrid(numCols) {
-		let colIndex = Math.floor(2 * (numCols - config.minCols) / config.colStep);
-		let rowIndex = colIndex + 1;
-		for (let projectData of projects) {
-			let ele = projectEles[projectData.id];
-			ele.style.left = (projectData.coordinates[colIndex] * (config.cellWidth + config.cellGap)) + 'px';
-			ele.style.top = (projectData.coordinates[rowIndex] * (config.cellHeight + config.cellGap)) + 'px';
+		var colIndex = Math.floor(2 * (numCols - config.minCols) / config.colStep);
+		var rowIndex = colIndex + 1;
+		for (var project in projects) {
+			var id = projects[project][0];
+			var coordinates = projects[project][1];
+			var ele = projectEles[id];
+			ele.style.left = (coordinates[colIndex] * (config.cellWidth + config.cellGap)) + 'px';
+			ele.style.top = (coordinates[rowIndex] * (config.cellHeight + config.cellGap)) + 'px';
 		}
 		galleryEle.style.width = (numCols * config.cellWidth + (numCols - 1) * config.cellGap) + 'px';
 		galleryEle.style.height = gridHeights[Math.floor((numCols - config.minCols) / config.colStep)] + 'px';
 	}
 
-	// on window resize, reposition all grid images
-	let resizeTimer = null;
-	window.addEventListener("resize", () => {
+	// // on window resize, reposition all grid images
+	var resizeTimer = null;
+	window.addEventListener("resize", function() {
 		if (!resizeTimer) {
-			resizeTimer = setTimeout(() => {
+			resizeTimer = setTimeout(function() {
 				resizeTimer = null;
 				considerAdjustingGrid();
 			}, 100);
@@ -47,18 +52,20 @@ window.addEventListener('load', () => {
 	}, false);
 
 	// on pageload, load all animated images
-	for (let projectData of animatedProjects) {
-		let id = projectData.id;
-		let scale = projectData.scale;
-		let img = new Image();
-		img.onload = () => {
-			img.width *= scale;
-			img.height *= scale;
-			let projectImageEle = projectEles[id].getElementsByClassName('gallery-item-image');
-			projectEles[id].removeChild(projectImageEle[0]);
-			projectEles[id].appendChild(img);
-		};
-		img.src = projectData.imageUri
+	for (project in animatedProjects) {
+		(function(projectData) {
+			var id = projectData[0];
+			var scale = projectData[1];
+			var img = new Image();
+			img.onload = function() {
+				img.width *= scale;
+				img.height *= scale;
+				var projectImageEle = projectEles[id].getElementsByClassName('gallery-item-image');
+				projectEles[id].removeChild(projectImageEle[0]);
+				projectEles[id].appendChild(img);
+			};
+			img.src = projectData[2];
+		})(animatedProjects[project]);
 	}
 
 	// consider adjusting the grid immediately on pageload
